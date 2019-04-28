84650e7dcccd596c4ae3bf6d566106754c9f8471
tombiasz <tomasz.m.nowacki@gmail.com>
2019-04-28 18:39:09 +0200

    add Email entity

---
+++ src/email.js
@@ -0,0 +1,29 @@
+const tlds = require('tlds');
+
+const EMAIL_HOSTNAME_REGEX = /[^@]+$/
+
+class Email {
+  constructor({
+    id,
+    email,
+    createdAt = Date.now(),
+    deletedAt = null,
+  }) {
+    this.id = id;
+    this.email = email;
+    this.createdAt = createdAt;
+    this.deletedAt = deletedAt;
+  }
+
+  get isTld() {
+    const hostname = this.email.match(EMAIL_HOSTNAME_REGEX).pop();
+    const domain = hostname.substring(hostname.indexOf('.') + 1);
+    return tlds.indexOf(domain) !== -1;
+  }
+
+  get isDeleted() {
+    return this.deletedAt !== null;
+  }
+}
+
+module.exports = Email;
