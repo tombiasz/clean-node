4b69fabdccda0cf7578c5292608a10562f38db86
tombiasz <tomasz.m.nowacki@gmail.com>
2019-05-03 13:58:00 +0200

    add ContactsRepository

---
+++ src/dataProviders/repositories/ContactsRepository.js
@@ -0,0 +1,10 @@
+/* eslint-disable class-methods-use-this */
+/* eslint-disable no-unused-vars */
+
+class ContactsRepository {
+  getById(id) {
+    throw new Error('implement me');
+  }
+}
+
+module.exports = ContactsRepository;
