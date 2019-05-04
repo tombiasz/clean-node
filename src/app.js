f6a8e2e8e392dc36285a33dc43a728ea503cec08
tombiasz <tomasz.m.nowacki@gmail.com>
2019-05-04 16:55:34 +0200

    add contacts db config

--- src/app.js
+++ src/app.js
@@ -3,7 +3,5 @@ const makeRestServer = require('./entrypoints/rest/server');
 
 const config = makeConfig(process.env);
 
-if (config.restApiEnabled) {
-  makeRestServer(config.restApi)
-    .then(port => console.log(`REST Api listening on port ${port}!`));
-}
+makeRestServer(config.restApi)
+  .then(port => console.log(`REST Api listening on port ${port}!`));
--- src/config.js
+++ src/config.js
@@ -1,8 +1,16 @@
 const toBool = value => value === 'true';
 
-module.exports = env => ({
-  restApiEnabled: toBool(env.REST_API_ENABLED),
-  restApi: {
-    port: env.REST_API_PORT,
-  },
+const makeRestApiConfig = env => Object.freeze({
+  port: env.REST_API_PORT,
+});
+
+const makeContactsDbConfig = env => Object.freeze({
+  dbClient: env.CONTACTS_DB_CLIENT,
+  dbFilename: env.CONTACTS_DB_FILENAME,
+  dbLogging: toBool(env.CONTACTS_DB_LOGGING),
+});
+
+module.exports = env => Object.freeze({
+  restApi: makeRestApiConfig(env),
+  contactsDb: makeContactsDbConfig(env),
 });
