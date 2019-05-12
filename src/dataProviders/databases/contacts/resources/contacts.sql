5e85b4c1e1d872420e76d5a1561f491175ecaf32
tombiasz <tomasz.m.nowacki@gmail.com>
2019-05-12 16:25:37 +0200

    add contacts_emails table to db schema

--- src/dataProviders/databases/contacts/resources/contacts.sql
+++ src/dataProviders/databases/contacts/resources/contacts.sql
@@ -1,5 +1,4 @@
 DROP TABLE IF EXISTS users;
-
 CREATE TABLE users (
   id INT PRIMARY KEY,
   username VARCHAR(50) NOT NULL,
@@ -10,8 +9,8 @@ CREATE TABLE users (
 
 INSERT INTO users (id, username) VALUES (1, "user1");
 
-DROP TABLE IF EXISTS contacts;
 
+DROP TABLE IF EXISTS contacts;
 CREATE TABLE contacts (
   id INT PRIMARY KEY,
   first_name VARCHAR(50) NOT NULL,
@@ -25,11 +24,30 @@ CREATE TABLE contacts (
 INSERT INTO contacts (id, first_name, last_name) VALUES (1, "Fizz", "Buzz");
 INSERT INTO contacts (id, first_name, last_name) VALUES (2, "Foo", "Bar");
 
-DROP TABLE IF EXISTS emails;
 
+DROP TABLE IF EXISTS emails;
 CREATE TABLE emails (
   id INT PRIMARY KEY,
   email VARCHAR(50) NOT NULL,
   created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
   deleted_at DATETIME DEFAULT NULL
 );
+
+INSERT INTO emails (id, email) VALUES (1, "foo@bar");
+INSERT INTO emails (id, email) VALUES (2, "foo@bar.com");
+INSERT INTO emails (id, email) VALUES (3, "fizz@buzz");
+
+
+DROP TABLE IF EXISTS contacts_emails;
+CREATE TABLE contacts_emails (
+  contact_id INT NOT NULL,
+  email_id INT NOT NULL,
+  created_at DATETIME DEFAULT CURRENT_TIMESTAMP,
+  FOREIGN KEY (contact_id) REFERENCES contacts(id),
+  FOREIGN KEY (email_id) REFERENCES emails(id),
+  PRIMARY KEY (contact_id, email_id)
+);
+
+INSERT INTO contacts_emails (contact_id, email_id) VALUES (2, 1);
+INSERT INTO contacts_emails (contact_id, email_id) VALUES (2, 2);
+INSERT INTO contacts_emails (contact_id, email_id) VALUES (1, 3);
